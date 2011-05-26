3a92015f22e54798994a9b9f045fb8acd48e60f8
Rodrigo Dueñas <rduenasf@gmail.com>
2011-05-26 12:11:42 -0400

    Agregando material para auxiliar 3, año 2011

---
+++ 2011/Auxiliar 3/media/java/postsPrueba.js
@@ -0,0 +1,8 @@
+module.exports = [
+{title: "Primer post", body: "Este es el texto del primer post"},
+{title: "Segundo post", body: "Este es el texto del segundo post"},
+{title: "Tercer post", body: "Este es el texto del tercer post"},
+{title: "Cuarto post", body: "Este es el texto del cuarto post"},
+{title: "Quinto post", body: "Este es el texto del quinto post"}
+];
+
